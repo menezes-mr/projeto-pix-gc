c96366d88e946b3d55932447b37fc580995825ee
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-08-28 18:10:42 -0300

    feat: implementa fluxo completo de cadastro de usuario

---
+++ backend/src/usuarios/dto/create-usuario.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  Matches,
+  ValidateIf,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateUsuarioDto {
+  @IsString()
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  nomeCompleto!: string;
+
+  @IsEmail({}, { message: 'Formato de e-mail inválido' })
+  @IsNotEmpty({ message: 'O e-mail é obrigatório' })
+  email!: string;
+
+  @IsString({ message: 'O CPF/CNPJ deve ser um texto' })
+  @IsNotEmpty({ message: 'O CPF/CNPJ é obrigatório' })
+  @Matches(/^(\d{11}|\d{14})$/, {
+    message:
+      'O CPF deve ter 11 dígitos ou o CNPJ deve ter 14 dígitos (apenas números)',
+  })
+  cpfCnpj!: string;
+
+  @ValidateIf((_, value) => value !== null)
+  @IsString({ message: 'O telefone deve ser do tipo texto' })
+  @IsOptional()
+  telefone?: string | null;
+
+  @IsString()
+  @IsNotEmpty({ message: 'A senha é obrigatória' })
+  @MinLength(8, { message: 'A senha deve ter no mínimo 8 caracteres' })
+  senha!: string;
+}
