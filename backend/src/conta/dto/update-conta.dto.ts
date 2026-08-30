a8f4a6733fb213f5172ab9cb2de6778bc743ef46
Marcelo Menezes <menzs.marcelo@gmail.com>
2026-08-29 22:02:03 -0300

    chore: reescrevendo update-conta.dto.ts usando PartialType

--- backend/src/conta/dto/update-conta.dto.ts
+++ backend/src/conta/dto/update-conta.dto.ts
@@ -1,12 +1,4 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateContaDto } from './create-conta.dto';
 
-export class UpdateContaDto {
-  @IsNotEmpty({ message: 'O ID do usuário é obrigatório.' })
-  @IsString()
-  usuarioId!: string;
-
-  @IsOptional()
-  @IsNumber({}, { message: 'O limite diário do PIX deve ser um valor numérico.' })
-  @Min(0, { message: 'O limite diário do PIX não pode ser negativo.' })
-  limiteDiarioPix?: number;
-}
+export class UpdateContaDto extends PartialType(CreateContaDto) {}
