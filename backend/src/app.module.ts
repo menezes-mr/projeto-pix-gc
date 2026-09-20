55d9afdc65f6ecac3fb4da46d615bcfc48f47c2d
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-09-20 11:19:33 -0300

    feat: instala e configura EventEmitterModule no AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ContaModule } from './conta/conta.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PixKeyModule } from './pix-key/pix-key.module';
@@ -8,6 +9,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     PrismaModule,
     ContaModule,
     UsuariosModule,
