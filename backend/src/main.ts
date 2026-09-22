127411908e0d3cb52631e8f109cf85f3137e59bf
Joao Victor Verissimo <joaovictorverissimo20@gmail.com>
2026-09-22 18:59:35 -0300

    feat(main): adicionando swagger para fazer teste das rotas

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Projeto PIX GC')
+    .setDescription('API do sistema de pagamentos PIX')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3333);
 }
-bootstrap();
+bootstrap().catch(console.error);
