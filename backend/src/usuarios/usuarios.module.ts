dd2a295df2a51e4b3d01eb329f2c53d118e119a3
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-09-21 22:49:56 -0300

    feat: registra relatório de transações no módulo de usuários

--- backend/src/usuarios/usuarios.module.ts
+++ backend/src/usuarios/usuarios.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { RelatorioTransacoesService } from './relatorio-transacoes.service';
+import { RelatorioTransacoesController } from './relatorio-transacoes.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [UsuariosController],
-  providers: [UsuariosService],
+  imports: [PrismaModule, JwtModule.register({})],
+  controllers: [UsuariosController, RelatorioTransacoesController],
+  providers: [UsuariosService, RelatorioTransacoesService],
 })
 export class UsuariosModule {}
